306fc018d28ced4ad3308b01bc47a9fe0bdf117c
arthurshime3 <arthur.shim.e3@gmail.com>
2018-03-26 17:43:23 -0400

    Just making sure I don't lose anything

--- Protecc/app/src/main/java/com/wedemkois/protecc/controllers/ShelterDetailActivity.java
+++ Protecc/app/src/main/java/com/wedemkois/protecc/controllers/ShelterDetailActivity.java
@@ -91,8 +91,10 @@ public class ShelterDetailActivity extends AppCompatActivity {
     }
 
     public void onShelterLoaded(Shelter shelter) {
+        currentShelter = shelter;
+
         shelterNameTextView.setText(shelter.getName());
-        shelterCapacityTextView.setText(shelter.getCapacity());
+        updateCapacityTextView();
         shelterAgeGroupTextView.setText(shelter.getAgeRange());
         shelterGenderTextView.setText(shelter.getGender());
         shelterChildrenTextView.setText(shelter.isChildrenAllowed() ? "Children allowed" : "Children not allowed");
@@ -101,7 +103,110 @@ public class ShelterDetailActivity extends AppCompatActivity {
         shelterAddressTextView.setText(shelter.getAddress());
         shelterNotesTextView.setText(String.join(",", shelter.getNotes()));
         shelterPhoneTextView.setText(shelter.getPhoneNumber());
+    }
 
+    public boolean checkQualifications(String[] ageGroup, String[] gender, boolean childrenAllowed) {
+        if (!(currentShelter.getAgeRange().equals("All"))) {
+            for (int i = 0; i < ageGroup.length; i++) {
+                if (!(ageGroup[i].equals(currentShelter.getAgeRange()))) {
+                    return false;
+                }
+            }
+        }
+        if (!(currentShelter.getGender().equals("Both"))) {
+            for (int i = 0; i < gender.length; i++) {
+                if (!(gender[i].equals(currentShelter.getGender()))) {
+                    return false;
+                }
+            }
+        }
+        if (!(childrenAllowed == currentShelter.isChildrenAllowed())) return false;
+        return true;
+    }
 
+    /*
+    * Method that updates number of vacant beds at the shelter if possible.
+    * @param users can be positive (checking in) or negative (checking out)
+    * @param group true if a "group bed" is updating its vacancy
+    * @return boolean array of length 2, with index 0 being true if check-in/out was valid, false if not
+    *   and index 1 being true if the check in/out was a type group, false for type individual
+    */
+    public boolean[] updateVacancy(int users, boolean group) {
+        if (users == 1 && group)
+            throw new IllegalArgumentException("Error: A single user checking in is not a group");
+        if (users > 1 && !group)
+            throw new IllegalArgumentException("Error: More than one user checking in is a group");
+
+        boolean[] output = {false, group};
+        if (users == 0) {
+            output[0] = true;
+            return output;
+        } else if (!group)  // 1 user checking in or any number checking out
+        {
+            int bedsTaken = Integer.parseInt(currentShelter.getIndividualBedsTaken());
+            if (bedsTaken == 0 && users < 0) {
+                output[0] = false;
+                return output;
+            }
+            int vacancies = Integer.parseInt(currentShelter.getIndividualCapacity()) - bedsTaken;
+            if (vacancies >= users) {
+                currentShelter.setIndividualBedsTaken((Integer.parseInt(currentShelter.getIndividualBedsTaken()) + users) + "");
+                updateCapacityTextView();
+                output[0] = true;
+                return output;
+            }
+            else    // individual beds full, check group beds (this code is only reached for checking in)
+            {
+                bedsTaken = Integer.parseInt(currentShelter.getGroupBedsTaken());
+                vacancies = Integer.parseInt(currentShelter.getGroupCapcity()) - bedsTaken;
+
+                if (vacancies > 0)  // check for open group beds
+                {
+                    currentShelter.setGroupBedsTaken((Integer.parseInt(currentShelter.getGroupBedsTaken()) + 1) + "");
+                    updateCapacityTextView();
+                    output[0] = true;
+                    return output;
+                }
+
+                return output;  // no beds found
+            }
+        } else {    // group checking in or any number checking out
+            output[1] = true;
+            int bedsTaken = Integer.parseInt(currentShelter.getGroupBedsTaken());
+            if (bedsTaken == 0 && users < 0) {
+                output[0] = false;
+                return output;
+            }
+            int vacancies = Integer.parseInt(currentShelter.getGroupCapcity()) - bedsTaken;
+            if (vacancies >= users) {
+                currentShelter.setIndividualBedsTaken((Integer.parseInt(currentShelter.getIndividualBedsTaken()) + users) + "");
+                updateCapacityTextView();
+                output[0] = true;
+                return output;
+            }
+            else    // group beds full, check individual beds (this code is only reached for checking in
+            {
+                bedsTaken = Integer.parseInt(currentShelter.getIndividualBedsTaken());
+                vacancies = Integer.parseInt(currentShelter.getIndividualCapacity()) - bedsTaken;
+
+                if (vacancies >= users)
+                {
+                    currentShelter.setIndividualBedsTaken((Integer.parseInt(currentShelter.getIndividualBedsTaken()) + users) + "");
+                    updateCapacityTextView();
+                    output[0] = true;
+                    return output;
+                }
+
+                return output;  // no beds found
+            }
+        }
+    }
+
+    private void updateCapacityTextView()
+    {
+        int capacity = Integer.parseInt(currentShelter.getIndividualCapacity()) - Integer.parseInt(currentShelter.getIndividualBedsTaken())
+                + (Integer.parseInt(currentShelter.getGroupCapcity()) - Integer.parseInt(currentShelter.getGroupBedsTaken()) * 4);
+
+        shelterCapacityTextView.setText(capacity + "");
     }
 }
