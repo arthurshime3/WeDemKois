b89f8ab1d121a8b71f93b5947f85779a433a8b1a
Jeffrey Minowa <jeffminowa@gatech.edu>
2018-04-16 01:12:16 -0400

    Changed CheckQualifications and made JUnit for it

---
+++ Protecc/app/src/test/java/com/wedemkois/protecc/CheckQualificationsTest.java
@@ -0,0 +1,68 @@
+package com.wedemkois.protecc;
+
+import org.junit.Test;
+import com.wedemkois.protecc.model.Shelter;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class CheckQualificationsTest {
+
+    Shelter shelterALL = new Shelter("", "", "",
+            "", "", "ALL",
+            "BOTH", "T", "",
+            "0", "0", "", "", "");
+
+
+    private String[] ageGroups = new String[3];
+
+    String[] genders = new String[2];
+    boolean childrenAllowed = true;
+
+    @Test
+    public void checkBasicQualificationsTest() {
+        assertEquals(shelterALL.checkQualifications(ageGroups, genders, childrenAllowed), true);
+    }
+
+    @Test
+    public void checkAdultOnlyQualificationsTest() {
+        ageGroups[0] = Shelter.AgeRange.ADULT.name();
+        ageGroups[1] = Shelter.AgeRange.YOUNGADULTS.name();
+        ageGroups[2] = Shelter.AgeRange.FAMILY.name();
+        genders[0] = Shelter.Gender.BOTH.name();
+        genders[1] = Shelter.Gender.MEN.name();
+        shelterALL.setAgeRange(Shelter.AgeRange.ADULT.name());
+        shelterALL.setChildrenAllowed(false);
+
+        assertEquals(shelterALL.checkQualifications(ageGroups, genders, childrenAllowed), false);
+    }
+
+    @Test
+    public void checkFamilyQualificationsTest() {
+        ageGroups[0] = Shelter.AgeRange.ALL.name();
+        ageGroups[1] = Shelter.AgeRange.YOUNGADULTS.name();
+        ageGroups[2] = Shelter.AgeRange.FAMILY.name();
+        shelterALL.setAgeRange(Shelter.AgeRange.FAMILY.name());
+        shelterALL.setGender(Shelter.Gender.BOTH.name());
+        shelterALL.setChildrenAllowed(true);
+        assertEquals(shelterALL.checkQualifications(ageGroups, genders, childrenAllowed), true);
+    }
+
+    @Test
+    public void checkMenOnlyQualificationsTest() {
+        ageGroups[0] = Shelter.AgeRange.ALL.name();
+        ageGroups[1] = Shelter.AgeRange.YOUNGADULTS.name();
+        ageGroups[2] = Shelter.AgeRange.FAMILY.name();
+        genders[0] = Shelter.Gender.MEN.name();
+        genders[1] = Shelter.Gender.MEN.name();
+        shelterALL.setAgeRange("ADULT");
+        shelterALL.setChildrenAllowed(false);
+        childrenAllowed = false;
+        assertEquals(shelterALL.checkQualifications(ageGroups, genders, childrenAllowed), true);
+
+    }
+}
